b7d339193963986a46ff645d75475cf687eb432b
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-03-03 20:43:44 +0100

    second commit

--- sample.java
+++ sample.java
@@ -1,9 +1,26 @@
-/**
- * sample
- */
-public class sample {
+class sample {
+    public static void main (String[] args) {
+    // for
+    for (int i = 1; i <= 5; i++) {
+    System.out.println(i);
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello Worl");
+    // while
+    int j = 1;
+    while (j <=5) {
+        System.out.println(j);
+        j++;
+    }
+    // do while
+    int k = 1;
+    do {
+        System.out.println(k);
+        k++;
+    } while (k <=1);
+    }
+
+    @Override
+    public String toString() {
+        return "sample []";
     }
 }
